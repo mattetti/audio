67eca05d5a58791e6ca0cfe391be7fd9519c581a
Matt Aimonetti <mattaimonetti@gmail.com>
2014-10-12 19:54:23 -0700

    added package documentation to the aiff pkg

---
+++ aiff/doc.go
@@ -0,0 +1,7 @@
+/*
+Package aiff is a AIFF/AIFC parser that extracts the basic information and allows developers to customize the parsing experienced.
+
+Besides the parsing functionaity, this package also provides a way for developers to access the duration of an aiff/aifc file.
+
+*/
+package aiff
