5ff62e1ff386e4e35a7cba2de8a073192df0fe2d
Matt Aimonetti <mattaimonetti@gmail.com>
2015-09-29 23:30:28 -0700

    documentation formatting

--- riff/wav/wav.go
+++ riff/wav/wav.go
@@ -1,5 +1,6 @@
 /*
-wav is a package allowing developers to decode and encode audio PCM
+
+Package wav is a package allowing developers to decode and encode audio PCM
 data using the Waveform Audio File Format https://en.wikipedia.org/wiki/WAV
 
 wav is a subformat of the riff tag format.
