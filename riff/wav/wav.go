738e4c8e61a70be33a71347a545aa9b06d8fe457
Matt Aimonetti <mattaimonetti@gmail.com>
2016-07-11 12:31:08 -0600

    deprecate riff/wav

--- riff/wav/wav.go
+++ riff/wav/wav.go
@@ -1,9 +1,6 @@
 /*
 
-Package wav is a package allowing developers to decode and encode audio PCM
-data using the Waveform Audio File Format https://en.wikipedia.org/wiki/WAV
-
-wav is a subformat of the riff tag format.
+DEPRECATED PACKAGE, see github.com/mattetti/audio/wav
 
 */
 package wav
