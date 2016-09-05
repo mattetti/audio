c99428776ad9d99557d138cd84bfa2449186fcfc
Matt Aimonetti <mattaimonetti@gmail.com>
2016-09-04 22:54:40 -0700

    analysis: constant Q implementation

--- dsp/analysis/constantq.go
+++ dsp/analysis/constantq.go
@@ -1,6 +1,11 @@
 package analysis
 
-import "math"
+import (
+	"go-dsp/fft"
+	"math"
+
+	"github.com/mattetti/audio"
+)
 
 // ConstantQConfig is the config used to do a Constant Q transform
 // it defines the geometrically-spaced frequency range to analyze.
@@ -15,14 +20,6 @@ type ConstantQConfig struct {
 	BinsPerOctave int
 	// Threshold is the sensitivity threshold (suggested: 0.0054)
 	Threshold float64
-}
-
-// Speckernel is a spectral kernel matrix.
-type Speckernel struct {
-	IS   []int
-	JS   []int
-	Imag []float64
-	Real []float64
 }
 
 // ConstantQ transform transforms a data series to the frequency domain.
@@ -45,8 +42,10 @@ type ConstantQ struct {
 	// FFTLen is the length of the FFT required for the filter bank
 	FFTLen int
 	// Q value of the filter bank
-	Q       float64
-	HopSize int
+	Q          float64
+	K          int
+	HopSize    int
+	Speckernel *Speckernel
 }
 
 // NewConstantQ configures and returns a constant Q transform ready to be used.
@@ -67,5 +66,144 @@ func NewConstantQ(config *ConstantQConfig) *ConstantQ {
 		))
 	cq.HopSize = int(float64(cq.FFTLen) / 8)
 	cq.Data = make([]float64, 2*cq.ConstantQBins)
+	cq.K = int(
+		math.Ceil(
+			float64(cq.ConstantQBins) *
+				math.Log(config.MaxFs/config.MinFs) /
+				math.Log(2.0),
+		))
 	return cq
+}
+
+func (cq *ConstantQ) Process(buf *audio.PCMBuffer) []float64 {
+	if cq == nil {
+		return nil
+	}
+
+	fftData := fft.FFTReal(buf.AsFloat64s())
+
+	if cq.Speckernel == nil {
+		cq.NewSpeckernel()
+	}
+	for row := 0; row < 2*cq.K; row++ {
+		cq.Data[row] = 0
+		cq.Data[row+1] = 0
+	}
+
+	fftbin := cq.Speckernel.IS
+	cqbin := cq.Speckernel.JS
+	reals := cq.Speckernel.Real
+	imags := cq.Speckernel.Imag
+	sparseCells := len(cq.Speckernel.Real)
+
+	var (
+		row, col       int
+		r1, r2, i1, i2 float64
+	)
+	for i := 0; i < sparseCells; i++ {
+		row = cqbin[i]
+		col = fftbin[i]
+		r1 = reals[i]
+		i1 = imags[i]
+		r2 = real(fftData[(2*cq.FFTLen)-2*col-2])
+		i2 = imag(fftData[(2*cq.FFTLen)-2*col-2+1])
+		cq.Data[2*row] += (r1*r2 - i1*i2)
+		cq.Data[2*row+1] += (r1*r2 + i1*i2)
+	}
+
+	return cq.Data
+}
+
+// NewSpeckernel generates a new spectral kernel matrix
+func (cq *ConstantQ) NewSpeckernel() error {
+	if cq == nil {
+		return nil
+	}
+	cq.Speckernel = &Speckernel{}
+
+	hammingWindowRe := make([]float64, cq.FFTLen)
+	hammingWindowIm := make([]float64, cq.FFTLen)
+
+	for u := 0; u < cq.FFTLen; u++ {
+		hammingWindowRe[u] = 0
+		hammingWindowIm[u] = 0
+	}
+
+	// for each bin value K, calculate temporal kernel, take its fft to
+	// calculate the spectral kernel then threshold it to make it sparse and
+	// add it to the sparse kernels matrix
+	squareThreshold := cq.Config.Threshold * cq.Config.Threshold
+
+	for k := cq.K; k > 0; k-- {
+		for u := 0; u < cq.FFTLen; u++ {
+			hammingWindowRe[u] = 0
+			hammingWindowIm[u] = 0
+		}
+
+		// Computing a hamming window
+		hammingLength :=
+			math.Ceil(
+				cq.Q * cq.Config.Fs /
+					(cq.Config.MinFs *
+						math.Pow(2,
+							(cq.Q)/float64(cq.ConstantQBins))),
+			)
+
+		origin := cq.FFTLen/2 - int(hammingLength)/2
+
+		for i := 0; i < int(hammingLength); i++ {
+			angle := 2 * math.Pi * cq.Q * float64(i) / float64(hammingLength)
+			real := math.Cos(angle)
+			imag := math.Sin(angle)
+			absol := hamming(hammingLength, float64(i)) / hammingLength
+			hammingWindowRe[origin+i] = absol * real
+			hammingWindowIm[origin+i] = absol * imag
+		}
+
+		for i := 0; i < cq.FFTLen/2; i++ {
+			temp := hammingWindowRe[i]
+			hammingWindowRe[i] = hammingWindowRe[i+cq.FFTLen/2]
+			hammingWindowRe[i+cq.FFTLen/2] = temp
+			temp = hammingWindowIm[i]
+			hammingWindowIm[i] = hammingWindowIm[i+cq.FFTLen/2]
+			hammingWindowIm[i+cq.FFTLen/2] = temp
+		}
+
+		hammingWindow := make([]complex128, len(hammingWindowRe))
+		for i := 0; i < len(hammingWindow); i++ {
+			hammingWindow[i] = complex(hammingWindowRe[i], hammingWindowIm[i])
+		}
+
+		transfHammingWindow := fft.FFT(hammingWindow)
+
+		for j := 0; j < cq.FFTLen; j++ {
+			// perform thresholding
+			squaredBin := transfHammingWindow[j] * transfHammingWindow[j]
+			if real(squaredBin) <= squareThreshold {
+				continue
+			}
+
+			// Insert non-zero position indexes, doubled because they are floats
+			cq.Speckernel.IS = append(cq.Speckernel.IS, j)
+			cq.Speckernel.JS = append(cq.Speckernel.JS, k)
+
+			// take conjugate, normalise and add to array sparkernel
+			cq.Speckernel.Real = append(cq.Speckernel.Real, real(transfHammingWindow[j])/float64(cq.FFTLen))
+			cq.Speckernel.Imag = append(cq.Speckernel.Imag, -imag(transfHammingWindow[j])/float64(cq.FFTLen))
+		}
+	}
+
+	return nil
+}
+
+// Speckernel is a spectral kernel matrix.
+type Speckernel struct {
+	IS   []int
+	JS   []int
+	Imag []float64
+	Real []float64
+}
+
+func hamming(len, n float64) float64 {
+	return 0.54 - 0.46*math.Cos(2*math.Pi*n/len)
 }
