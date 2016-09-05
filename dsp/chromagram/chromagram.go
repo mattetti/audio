8c78eda746ad3b5ec8a5c8e20a7a809e87e8bcb1
Matt Aimonetti <mattaimonetti@gmail.com>
2016-09-05 00:03:28 -0700

    chromagram: progress

--- dsp/chromagram/chromagram.go
+++ dsp/chromagram/chromagram.go
@@ -3,14 +3,19 @@
 // See https://en.wikipedia.org/wiki/Chroma_feature
 package chromagram
 
-import "github.com/mattetti/audio/dsp/analysis"
+import (
+	"github.com/mattetti/audio"
+	"github.com/mattetti/audio/dsp/analysis"
+	"github.com/mattetti/audio/dsp/windows"
+)
 
 type Chromagram struct {
 	constantQ *analysis.ConstantQ
 	Results   []float64
 	FrameSize int
 	HopSize   int
-	//FFT
+	FFTData   []complex128
+	Window    []float64
 }
 
 func New(config *analysis.ConstantQConfig) *Chromagram {
@@ -18,6 +23,30 @@ func New(config *analysis.ConstantQConfig) *Chromagram {
 	c.Results = make([]float64, config.BinsPerOctave)
 	c.FrameSize = c.constantQ.FFTLen
 	c.HopSize = c.constantQ.HopSize
-	// c.FFT of the framesize
+
 	return c
+}
+
+func (cg *Chromagram) Process(buf *audio.PCMBuffer) ([]float64, error) {
+	if cg.constantQ.Speckernel == nil {
+		if err := cg.constantQ.NewSpeckernel(); err != nil {
+			return nil, err
+		}
+	}
+
+	buf.SwitchPrimaryType(audio.Float)
+	windowBuf := make([]float64, cg.FrameSize)
+	if len(cg.Window) != cg.FrameSize {
+		cg.Window = windows.Hamming(cg.FrameSize)
+	}
+
+	// apply the window into a buffer
+	for i := 0; i < cg.FrameSize; i++ {
+		windowBuf[i] = buf.Floats[i] * cg.Window[i]
+	}
+
+	//data := fft.FFTReal(windowBuf)
+
+	// TODO: apply constant Q
+	return nil, nil
 }
